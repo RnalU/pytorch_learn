f9c1c1ccd1a959429d4d6f80fd618c6f0508d709
RnalU <ymc1415481736@outlook.com>
2023-05-28 10:04:01 +0800

    修改读取图片返回类型为"numpy.array"
    Signed-off-by: RnalU <Ymc1415481736@outlook.com>

--- day1/read_data.py
+++ day1/read_data.py
@@ -1,6 +1,6 @@
 import os
+import numpy as np
 from torch.utils.data import Dataset, DataLoader
-from PIL import Image
 import cv2 as cv
 
 
@@ -16,7 +16,11 @@ class MyDataSet(Dataset):
         self.img_name = self.img_list[index]
         self.img_path = os.path.join(self.load_path, self.img_name)
         self.img = cv.imread(self.img_path)
+        # 转换opencv的img通道
+        # self.img = cv.cvtColor(self.img, cv.COLOR_BGR2RGB)
+        self.img = np.array(self.img)
         self.img_label = self.label_path
+        print(self.img_name)
         return self.img, self.img_label
 
     def __len__(self):
@@ -24,7 +28,7 @@ class MyDataSet(Dataset):
 
 
 if __name__ == '__main__':
-    root_path = "./Data_set/hymenoptera_data/train"
+    root_path = "../Data_set/hymenoptera_data/train"
     label_path = "ants"
 
     MyDataSET = MyDataSet(root_path, label_path)
